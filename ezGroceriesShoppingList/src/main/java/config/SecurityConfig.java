bc6df78ae502f82d1ad1831ef1a512469199f413
Tami Apteker <tami.apteker@kbc.be>
2023-03-14 15:49:18 +0100

    started

---
+++ ezGroceriesShoppingList/src/main/java/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package config;
+
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+
+@Configuration
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authz) -> authz
+                .requestMatchers("/shopping-lists/**").authenticated());
+
+        return http.build();
+
+    }
+
+
+}
